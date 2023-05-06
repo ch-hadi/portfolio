28c791e02657b34bd841c233d3f92d245331cf9d
Ch_Hadi <49975653+ch-hadi@users.noreply.github.com>
2023-05-07 00:33:57 +0500

    Description changed

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -56,10 +56,10 @@ export const Banner = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
-                <h1>{`Hi! I'm Hammad Ahmad`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "MERN  Stack Engineer", "Software Developer" ]'><span className="wrap">{text}</span></span></h1>
-                <p>My fullname is Saidnazarova Odinanabonu. I'm 15 years old. I study 249-school in 10th grade.I am a front-end web developer. I can make the website more, more interactive with web animation
-           I study at Web Brain Academy group G5. When I first came to web brain, I had no knowledge of programming.
-           But now with the help of strong aspirations and teachers, my level of knowledge has greatly increased and I can freely create web site views that are used in our daily lives
+                <h1>{`Hi! I'm Hammad`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "MERN  Stack Engineer", "Software Developer" ]'><span className="wrap">{text}</span></span></h1>
+                <p>My fullname is Hammad Ahmad. I'm 23 years old. I study University of Gujrat 2019.I am a MERN Stack web developer. I can make the website more, more interactive with web animation
+           . When I first came to web brain, I had no knowledge of programming.
+           But now with the help of strong aspirations and self learning, my level of knowledge has greatly increased and I can freely create web site views that are used in our daily lives
         </p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
